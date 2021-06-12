da69cf892cf02b99995e15205e62238b5c860fe6
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-06-12 19:04:17 +0100

    .

--- src/http.js
+++ src/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const http = (_ =>
     axios.create({
-        baseURL: process.env.SERVER_URL || `http://localhost:3002`,
+        baseURL: process.env.SERVER_URL,
         Headers: {
             'Content-Type': 'application/json'
         }
