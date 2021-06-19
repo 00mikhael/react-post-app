28b9b81c6649c6930e10d905065db00a77515704
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-06-19 23:55:57 +0100

    add margin to dialog

--- src/components/fragments/PostDetail/PostDetail.js
+++ src/components/fragments/PostDetail/PostDetail.js
@@ -262,7 +262,7 @@ const PostDetail = () => {
                     <div className='flex items-center justify-center min-h-screen'>
                         <Dialog.Overlay className='fixed inset-0 bg-black opacity-30' />
 
-                        <div className=' bg-white rounded-lg max-w-sm px-6 py-4 space-y-2 shadow-lg z-10'>
+                        <div className=' bg-white rounded-lg max-w-sm m-4 px-6 py-4 space-y-2 shadow-lg z-10'>
                             <Dialog.Title className={`text-red-600 text-lg`}>
                                 Delete post
                             </Dialog.Title>
