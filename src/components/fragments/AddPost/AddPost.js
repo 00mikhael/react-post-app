f0bec2f723386c85225c53279dbb5e5ef4b89b1b
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-06-20 12:48:15 +0100

    add button hover states

--- src/components/fragments/AddPost/AddPost.js
+++ src/components/fragments/AddPost/AddPost.js
@@ -204,7 +204,7 @@ const AddPost = ({ isOpen, onClose }) => {
                         >
                             <button
                                 style={{ maxWidth: '8rem' }}
-                                className={`bg-purple-600 text-gray-50 w-full p-3 mt-3 rounded-md ${
+                                className={`bg-purple-600 hover:bg-purple-700 text-gray-50 w-full p-3 mt-3 rounded-md ${
                                     saving &&
                                     `opacity-50 cursor-not-allowed select-none`
                                 }`}
@@ -215,7 +215,7 @@ const AddPost = ({ isOpen, onClose }) => {
                             </button>
                             <button
                                 style={{ maxWidth: '8rem' }}
-                                className={`border border-gray-600 text-gray-600 w-full p-3 mt-3 rounded-md ${
+                                className={`border border-gray-500 text-gray-500 hover:text-gray-100 hover:bg-gray-500 w-full p-3 mt-3 rounded-md ${
                                     saving &&
                                     `opacity-50 cursor-not-allowed select-none`
                                 }`}
--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -25,7 +25,7 @@ const Dashboard = () => {
             if (!user) {
                 dispatch(showAuth(true))
             }
-        }, 3000)
+        }, 2000)
         return () => clearTimeout(timer)
         // eslint-disable-next-line
     }, [user])
