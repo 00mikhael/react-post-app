447e3b90627913cdc484ecbc8ddf5b11039a42eb
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-06-20 17:28:21 +0100

    add scroll top top

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 
+import ScrollToTop from './components/hooks/ScrollToTop'
+
 import Base from './components/sections/Base'
 import Header from './components/sections/Header'
 import Main from './components/sections/Main'
@@ -46,18 +48,20 @@ function App() {
 
     return (
         <Router>
-            <Base className={`min-h-screen opacity-100`}>
-                <Header
-                    className={`header bg rounded-bl-3xl  row-start-1 row-end-3 col-start-1 col-end-2`}
-                />
+            <ScrollToTop>
+                <Base className={`min-h-screen opacity-100`}>
+                    <Header
+                        className={`header bg rounded-bl-3xl  row-start-1 row-end-3 col-start-1 col-end-2`}
+                    />
 
-                <Main
-                    className={`row-start-2 row-end-4 col-start-1 col-end-2 z-10 min-h-screen mt-24`}
-                />
-                <Footer
-                    className={`row-start-4 row-end-5 col-start-1 col-end-2 mt-6`}
-                />
-            </Base>
+                    <Main
+                        className={`row-start-2 row-end-4 col-start-1 col-end-2 z-10 min-h-screen mt-24`}
+                    />
+                    <Footer
+                        className={`row-start-4 row-end-5 col-start-1 col-end-2 mt-6`}
+                    />
+                </Base>
+            </ScrollToTop>
         </Router>
     )
 }
